85e7472faec2347fd02a4400fdec2673d808cfaf
EugeneGpil <gpilligrim@gmail.com>
2026-07-19 23:15:11 +0700

    1

--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="column items-center justify-center q-pa-lg" style="min-height: 100vh">
-    <div class="text-h4 text-weight-bold q-mb-xl">Forest</div>
+    <div class="text-h4 text-weight-bold q-mb-xl">Shopping list</div>
 
     <q-btn
       class="full-width q-mb-md"
@@ -14,24 +14,12 @@
       <q-icon name="img:icons/google.svg" size="20px" class="q-mr-sm" />
       Continue with Google
     </q-btn>
-
-    <q-btn
-      class="full-width"
-      color="blue-8"
-      size="lg"
-      unelevated
-      rounded
-      @click="loginWithFacebook"
-    >
-      <q-icon name="img:icons/facebook.svg" size="20px" class="q-mr-sm" />
-      Continue with Facebook
-    </q-btn>
   </div>
 </template>
 
 <script setup>
 import { auth } from 'src/boot/firebase'
-import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from 'src/stores/auth'
 import { watch } from 'vue'
@@ -46,17 +34,5 @@ watch(() => authStore.isLoggedIn, (loggedIn) => {
 async function loginWithGoogle() {
   const provider = new GoogleAuthProvider()
   await signInWithPopup(auth, provider).catch(() => {})
-}
-
-async function loginWithFacebook() {
-  const provider = new FacebookAuthProvider()
-  provider.addScope('public_profile')
-  try {
-    await signInWithPopup(auth, provider)
-  } catch (e) {
-    if (e.code === 'auth/account-exists-with-different-credential') {
-      alert('An account already exists with this email. Please sign in with Google.')
-    }
-  }
 }
 </script>
